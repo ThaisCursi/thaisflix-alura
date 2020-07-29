23c41214543c7de02aac2ce40854d010da942801
ThaisCursi <thais.cursi@gmail.com>
2020-07-29 20:41:34 -0300

    Aula 2 -Imersão React

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/img/logo.png';
 import './menu.css';
 import Button from '../Button';
@@ -7,11 +8,11 @@ import Button from '../Button';
 function Menu(){
     return (
      <nav className="Menu">
-         <a href="/">
+      <Link to="/">
          <img src={logo} className="logo" alt="ThaisFlix"/>
-        </a>
+         </Link>
 
-        <Button as="a" className="ButtonLink" href="/">
+         <Button as={Link} className="ButtonLink" to="/cadastro/video">
             Novo video
         </Button>
      </nav>
